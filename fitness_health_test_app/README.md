41f2d7897fc5bd871e416c63b746e105a8f06c07
tran-haison <haison250199@gmail.com>
2021-11-13 17:13:11 +0700

    Init project

---
+++ fitness_health_test_app/README.md
@@ -0,0 +1,4 @@
+# fitness_health_test_app
+
+Fitness Health Test App with Real-time Pose Estimation
+
---
+++ fitness_health_test_app/android/app/src/main/kotlin/com/vigour/fitnesshealthtest/fitness_health_test_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vigour.fitnesshealthtest.fitness_health_test_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
